f651e92d1ac0726bab7d388614bd3dc1a7da9013
Dipu Dey <dipudey764@gmail.com>
2024-11-16 23:38:25 +0600

    :wrench: Update the setup file and docker compse file

--- bin/lib/setup.sh
+++ bin/lib/setup.sh
@@ -6,6 +6,11 @@ git clone https://github.com/dipudey/docker-laravel.git
 # Navigate into the directory
 cd docker-laravel
 
+# Set permission for execute
+chmod +x bin/*
+
+chmod 777 docker/phpmyadmin
+
 # Create the src directory if it doesn't exist
 mkdir -p src
 
@@ -14,3 +19,7 @@ bin/start --build
 
 # Run the bin/download script
 bin/download
+
+# Set the permission for the src folder
+sudo chmod u+rw src
+sudo chmod 777 src
