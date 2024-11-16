31214977c1b82684d06781fa14983b36fc89a800
Dipu Dey <dipudey764@gmail.com>
2024-11-16 22:52:54 +0600

    :rocket: add automatic stup file for laravel repository clone and download the laravel application

---
+++ bin/lib/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the repository (if not already cloned)
+git clone https://github.com/dipudey/docker-laravel.git
+
+# Navigate into the directory
+cd docker-laravel
+
+# Create the src directory if it doesn't exist
+mkdir -p src
+
+# Run the bin/download script
+bin/download
